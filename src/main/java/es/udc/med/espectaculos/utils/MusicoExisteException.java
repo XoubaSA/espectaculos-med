693955eab9171a54810d4f87a10278e18500866a
Pablo Paz <pablo.paz@udc.es>
2014-12-03 03:03:44 +0100

    me falto subir esta excepcion en el commit anterior

---
+++ src/main/java/es/udc/med/espectaculos/utils/MusicoExisteException.java
@@ -0,0 +1,14 @@
+package es.udc.med.espectaculos.utils;
+
+public class MusicoExisteException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5521005427991801540L;
+
+	public MusicoExisteException(String message) {
+		super(message);
+	}
+
+}
